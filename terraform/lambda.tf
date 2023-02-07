bb735356c5d14551e9dcce47ee015e0cdae1e402
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-07 21:11:05 +0530

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -13,7 +13,8 @@ resource "aws_lambda_function" "terraform_lambda_func" {
   function_name                  =  "welcome"
   role                           =  "${aws_iam_role.lambda_role.arn}"
   handler                        =  "welcome.hello"
-  source_code_hash               =   "${filebase64sha256(local.lambda_zip_location)}"
+  #source_code_hash               =   "${filebase64sha256(local.lambda_zip_location)}"
+  source_code_hash               =   "${data.archive_file.welcome.output_base64sha256}"
   runtime                        =  "python3.8"
 }
 
