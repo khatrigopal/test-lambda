e8acd9f6bd98272f9d12bb93fdb3cb0c4b6732ce
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-07 19:51:18 +0530

    Update lambda-iam.tf

--- terraform/iam/lambda-iam.tf
+++ terraform/iam/lambda-iam.tf
@@ -1,6 +1,7 @@
 ####
 resource "aws_iam_policy" "lambda policy" {
   name         = "lambda_policy"
+  role = "${aws_iam_role.test_role.id}"
   description  = "AWS IAM Policy for managing aws lambda role"
   role = "${file("iam/kambda-policy.json")}"
 }
