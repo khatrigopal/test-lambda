b95927e1f66b86486d2baa2941f64363f8a5cb67
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-07 20:24:37 +0530

    Create lambda.tf

---
+++ terraform/lambda.tf
@@ -0,0 +1,18 @@
+locals {
+  lambda_zip_location = "outputs/welcome.zip"
+}
+
+data "archive_file" "welcome" {
+  type        = "zip"
+  source_file = "welcome.py"
+  output_path = "${local.lmbda_zip_location}"
+}
+
+resource "aws_lambda_function" "terraform_lambda_func" {
+  filename                       =  "${local.lmbda_zip_location}"
+  function_name                  =  "welcome"
+  role                           =  "${aws_iam_role.lambda_role.arn}"
+  handler                        =  "welcome.hello"
+  runtime                        =  "python3.8"
+}
+
